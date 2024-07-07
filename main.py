602cd7fe124671d3b1860d001cdcc65f437ba3e2
JohnColeborn <jwcoleborn@gmail.com>
2024-07-07 13:28:44 +0800

    isolated code block producing bug - revising next

--- main.py
+++ main.py
@@ -33,37 +33,41 @@ def main():
             print("\n Please select a letter (a-z) or guess the word")
             
             while life >= 1: # set lives based on life to countdown
+                # WORD GUESS LOGIC
                 first_Guess = input("\n Enter your guess: ").lower()  # Prompt for new input, convert to lower string                
-                if len(first_Guess) > 1 and first_Guess == word: # if user guesses word early, Mad kudos
-                    make_a_guess (guessed_word, word, first_Guess) # takes guesser.py logic and adds other initilisations as specific arguments
-                    print(f"Wow, didn't expect you to guess {word} so easily! You brainiac you!")
-                    break
-                elif len(first_Guess) > 1 and first_Guess != word: # checks to see if input is a char(acceptable), or word (BREAK)
-                    make_a_guess (guessed_word, word, first_Guess) # takes guesser.py logic and adds other initilisations as specific arguments
-                    print(f"You entered more than one letter! And you guessed WRONG. The word was: {word}")
-                    break
-                elif first_Guess in word and len(first_Guess) == 1: # Correct Character Guess!!!
-                    if guessed_word == word: # IF word is guessed by char completion (short words only), end game in SUCCESS 
-                        print(f"Wow, didn't expect you to guess {word} so easily! You brainiac you!")
-                    else:                        
-                        life -= 1 # removes a life for guess
-                        make_a_guess (guessed_word, word, first_Guess) # takes guesser.py logic and adds other initilisations as specific arguments                    
-                        print(f"\n Correct Guess! Your word so far is {''.join(guessed_word)}, and you have {life} guesses remaining")
-                        
+                # if len(first_Guess) > 1 and first_Guess == word: # if user guesses word early, Mad kudos
+                #     make_a_guess (guessed_word, word, first_Guess) # takes guesser.py logic and adds other initilisations as specific arguments
+                #     print(f"Wow, didn't expect you to guess {word} so easily! You brainiac you!")
+                #     break
+                # elif len(first_Guess) > 1 and first_Guess != word: # checks to see if input is a char(acceptable), or word (BREAK)
+                #     make_a_guess (guessed_word, word, first_Guess) # takes guesser.py logic and adds other initilisations as specific arguments
+                #     print(f"You entered more than one letter! And you guessed WRONG. The word was: {word}")
+                #     break
 
-                else:
-                    life -= 1 # removes a life for guess
-                    print(f"\n Incorrect Guess! Your word so far is {''.join(guessed_word)}, and you have {life} guesses remaining")
+                # LETTER GUESS LOGIC
+                if first_Guess in word and len(first_Guess) == 1: # Correct Character Guess!!! 
+                    guessed_word = ''.join([c if c == first_Guess else gw for c, gw in zip(word, guessed_word)])  #Updates guessed word based on correct guess  
+                    print("above code words as intended")               
+                    if make_a_guess == word: # IF word is guessed by char completion (short words only), end game in SUCCESS !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                        print(f"Wow, didn't expect you to guess {word} so easily! You brainiac you!")
+                        return main()
+                    # else:                        
+                    #     life -= 1 # removes a life for guess
+                    #     make_a_guess (guessed_word, word, first_Guess) # takes guesser.py logic and adds other initilisations as specific arguments                    
+                    #     print(f"\n Correct Guess! Your word so far is {''.join(guessed_word)}, and you have {life} guesses remaining")
+                        
+                # END THE GAME LOGIC
+                # else:
+                #     life -= 1 # removes a life for guess
+                #     print(f"\n Incorrect Guess! Your word so far is {''.join(guessed_word)}, and you have {life} guesses remaining")
                     
-#  # Ends game when lives run out                               IF CHAR IS CORRECT FIX HERE OR NOT FIX HERE
-#                                             # Print word once life == 0
-#                     if life == 0 and word == make_a_guess:
-#                         print(f"Cracking Good Job for deducing: {word}")                              FIX THIS!!!!!!!!!!!!!
-#                     elif life == 0 and word != make_a_guess:
-#                         print(f"RIP, the word was: {word}")
-#                     else:
-#                         print(f"You've run out of guesses! The word was: {word}")
-#                         return main ()                    # if life == 0:
+                #     # Ends game when lives run out                                           
+                #     if life == 0: 
+                #         if word == make_a_guess and word and guessed_word: # Checks to see if word is correct on last character guess and SUCCESS
+                #             print(f"Cracking Good Job for deducing: {word}")   # Print word once life == 0                         
+                #         else:
+                #           print(f"You've run out of guesses! The word was: {word}") # Print word once life == 0
+                #         return main ()    # RESTARTS GAME               
         # END GAME CODE!
         elif choice == '2':
             print("Goodbye!")
