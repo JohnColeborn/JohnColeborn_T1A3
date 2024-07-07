30e0502514d5789f34fdee75d3c604b9f305459e
JohnColeborn <jwcoleborn@gmail.com>
2024-07-07 18:22:36 +0800

    portable hopefully

--- src/run.sh
+++ src/run.sh
@@ -1,1 +1,13 @@
+python3 -m venv virtualenv
+
+# Activate the virtual environment
+source virtualenv/bin/activate
+
+# Install the dependencies from requirements.txt
+pip install -r requirements.txt
+
+# Run the main application
 python3 main.py
+
+# Deactivate the virtual environment
+deactivate
